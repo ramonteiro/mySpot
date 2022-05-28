1b937c7da2363b5385fbb71edd0ea5d429cf3087
isaacpasc <72172489+isaacpasc@users.noreply.github.com>
2022-05-27 19:49:22 -0700

    fixed imsg transcript view

--- mySpotiMsg/MessagesViewController.swift
+++ mySpotiMsg/MessagesViewController.swift
@@ -20,10 +20,15 @@ class MessagesViewController: MSMessagesAppViewController, UITableViewDelegate, 
     var tableView = UITableView()
     var noSpotsMessage = UILabel()
     
+    var imageView = UIImageView()
+    weak var snapshotter: MKMapSnapshotter?
+    
+    var spotLocationCoord: CLLocationCoordinate2D?
+    var spotLocationName: String?
+    var spotLocationLocationName: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        syncFromAppGroups()
-        configureTableView()
     }
     
     // MARK: - UI
@@ -70,13 +75,106 @@ class MessagesViewController: MSMessagesAppViewController, UITableViewDelegate, 
         layout.subcaption = (entry.locationName.isEmpty ? "My Spot" : entry.locationName)
         layout.image = entry.image
         let message = MSMessage()
-        message.summaryText = "\(entry.x)+\(entry.y)+\(entry.name)"
-        message.layout = layout
+        var urlComponents = URLComponents()
+        urlComponents.queryItems = [URLQueryItem(name: "name", value: "\(entry.name)"), URLQueryItem(name: "x", value: "\(entry.x)"), URLQueryItem(name: "y", value: "\(entry.y)"), URLQueryItem(name: "location", value: "\(entry.locationName)")]
+        message.url = urlComponents.url
+        let liveLayout = MSMessageLiveLayout(alternateLayout: layout)
+        message.layout = liveLayout
         if (self.activeConversation != nil) {
             self.activeConversation?.insert(message, completionHandler: nil)
         } else {
             print("No convo")
         }
+    }
+    
+    func configureTranscriptView(url: URL?) {
+        guard let url = url else { return }
+        spotLocationCoord = CLLocationCoordinate2D(latitude: Double(url.valueOf("x") ?? "47.6153") ?? 47.6153, longitude: Double(url.valueOf("y") ?? "-122.33") ?? -122.33)
+        spotLocationName = url.valueOf("name")
+        spotLocationLocationName = url.valueOf("location")
+        view.addSubview(imageView)
+        view.addSubview(createPin())
+        view.addSubview(createBanner())
+        view.addSubview(createSubBanner())
+        imageView.frame = view.frame
+        imageView.pin(to: view)
+        if let spotLocationCoord = spotLocationCoord {
+            takeSnapshot(of: spotLocationCoord)
+        }
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    func createPin() -> UIView {
+        let pinImage = UIImageView(image: UIImage(systemName: "mappin"))
+        pinImage.tintColor = .red
+        pinImage.layer.zPosition = 100
+        pinImage.frame = CGRect(x: 217/2 - 15, y: 217/2 - 20, width: 30, height: 40)
+        return pinImage
+    }
+    
+    func createBanner() -> UIView {
+        let nameLabel = UILabel()
+        nameLabel.text = spotLocationName ?? "My Spot"
+        nameLabel.textAlignment = .center
+        nameLabel.numberOfLines = 0
+        nameLabel.adjustsFontSizeToFitWidth = true
+        nameLabel.textColor = .white
+        nameLabel.backgroundColor = UIColor.systemRed
+        nameLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        nameLabel.frame = CGRect(x: 0, y: 217 - 40, width: 217, height: 25)
+        nameLabel.layer.zPosition = 200
+        return nameLabel
+    }
+    
+    func createSubBanner() -> UIView {
+        let nameLabel = UILabel()
+        nameLabel.text = spotLocationLocationName ?? "?"
+        nameLabel.textAlignment = .center
+        nameLabel.textColor = .white
+        nameLabel.numberOfLines = 0
+        nameLabel.adjustsFontSizeToFitWidth = true
+        nameLabel.backgroundColor = UIColor.systemRed
+        nameLabel.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
+        nameLabel.frame = CGRect(x: 0, y: 217 - 15, width: 217, height: 15)
+        nameLabel.layer.zPosition = 300
+        return nameLabel
+    }
+    
+    @objc func didTap() {
+        guard let location = spotLocationCoord else { return }
+        guard let name = spotLocationName else { return }
+        let routeMeTo = MKMapItem(placemark: MKPlacemark(coordinate: location))
+        routeMeTo.name = name
+        routeMeTo.openInMaps(launchOptions: nil)
+    }
+    
+    override func contentSizeThatFits(_ size: CGSize) -> CGSize {
+        if presentationStyle != .compact && presentationStyle != .expanded {
+            let contentHeight: CGFloat = 217.0
+            return CGSize(width: contentHeight, height: contentHeight)
+        } else {
+            return size
+        }
+    }
+    
+    func takeSnapshot(of location: CLLocationCoordinate2D) {
+        snapshotter?.cancel()
+        
+        let options = MKMapSnapshotter.Options()
+        
+        options.region = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.007, longitudeDelta: 0.007))
+        options.mapType = .standard
+        options.showsBuildings = true
+        options.size = CGSize(width: 217, height: 217)
+        
+        let snapshotter = MKMapSnapshotter(options: options)
+        
+        snapshotter.start() { snapshot, _ in
+            self.imageView.image = snapshot?.image
+        }
+        
+        self.snapshotter = snapshotter
     }
     
     // MARK: - Data Handling
@@ -105,23 +203,37 @@ class MessagesViewController: MSMessagesAppViewController, UITableViewDelegate, 
     // MARK: - Conversation Handling
     
     override func willBecomeActive(with conversation: MSConversation) {
-        // Called when the extension is about to move from the inactive to active state.
-        // This will happen when the extension is about to present UI.
-        
-        // Use this method to configure the extension and restore previously stored state.
+        super.willBecomeActive(with: conversation)
+        syncFromAppGroups()
+        updatePresentation(for: conversation)
     }
     
-    override func willSelect(_ message: MSMessage, conversation: MSConversation) {
-        super.willSelect(message, conversation: conversation)
-        guard let locationArr: [String] = message.summaryText?.components(separatedBy: "+") else { return }
-        if locationArr.count > 1 {
-            let routeMeTo = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: Double(locationArr[0]) ?? 128.0, longitude: Double(locationArr[1]) ?? 129.0)))
-            if locationArr.count == 3 {
-                routeMeTo.name = locationArr[2]
-            } else {
-                routeMeTo.name = "My Spot"
-            }
-            routeMeTo.openInMaps(launchOptions: nil)
+    func updatePresentation(for conversation: MSConversation) {
+        if let url = conversation.selectedMessage?.url {
+            presentViewController(for: url, with: presentationStyle)
+        } else {
+            presentViewController(for: nil, with: presentationStyle)
+        }
+    }
+    
+    func presentViewController(for url: URL?, with presentationStyle: MSMessagesAppPresentationStyle) {
+        removeAllChildViewControllers()
+        
+        switch presentationStyle {
+        case .compact:
+            configureTableView()
+        case .expanded:
+            configureTableView()
+        case .transcript:
+            configureTranscriptView(url: url)
+        @unknown default:
+            configureTableView()
+        }
+    }
+    
+    func removeAllChildViewControllers() {
+        for view in self.view.subviews {
+            view.removeFromSuperview()
         }
     }
     
@@ -134,6 +246,8 @@ class MessagesViewController: MSMessagesAppViewController, UITableViewDelegate, 
         // and store enough state information to restore your extension to its current state
         // in case it is terminated later.
     }
+    
+    
     
     override func didReceive(_ message: MSMessage, conversation: MSConversation) {
         // Called when a message arrives that was generated by another instance of this
@@ -153,7 +267,6 @@ class MessagesViewController: MSMessagesAppViewController, UITableViewDelegate, 
     }
     
     override func willTransition(to newPresentationStyle: MSMessagesAppPresentationStyle) {
-        
     }
     
     override func didTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
@@ -161,7 +274,6 @@ class MessagesViewController: MSMessagesAppViewController, UITableViewDelegate, 
         
         // Use this method to finalize any behaviors associated with the change in presentation style.
     }
-    
 }
 
 class SpotCell: UITableViewCell {
@@ -232,5 +344,12 @@ extension UIView {
 extension String {
     func localized() -> String {
         return NSLocalizedString(self, tableName: "Localizable", bundle: .main, value: self, comment: self)
+    }
+}
+
+extension URL {
+    func valueOf(_ queryParameterName: String) -> String? {
+        guard let url = URLComponents(string: self.absoluteString) else { return nil }
+        return url.queryItems?.first(where: { $0.name == queryParameterName })?.value
     }
 }
