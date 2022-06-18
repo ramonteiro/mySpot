549fa2e6881c05712ef74814b7e33739c926e43b
isaacpasc <72172489+isaacpasc@users.noreply.github.com>
2022-06-18 13:57:08 -0700

    refactor

--- My Spot/View/Main View/PlaylistView.swift
+++ My Spot/View/Main View/PlaylistView.swift
@@ -14,61 +14,37 @@ import SwiftUI
 
 struct PlaylistView: View {
     
-    @FetchRequest(sortDescriptors: [
-        SortDescriptor(\.name)
-    ]) var playlists: FetchedResults<Playlist>
-    private let stack = CoreDataStack.shared
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.name)]) var playlists: FetchedResults<Playlist>
     @EnvironmentObject var mapViewModel: MapViewModel
     @EnvironmentObject var cloudViewModel: CloudKitViewModel
-    @State private var showingDeleteAlert = false
-    @State private var showingAddPlaylistSheet = false
+    @State private var presentDeleteAlert = false
+    @State private var presentAddPlaylistSheet = false
+    @State private var presentNoPermissionsAlert = false
     @State private var toBeDeleted: IndexSet?
-    @State private var showNoPermissionsAlert = false
     
     var body: some View {
         NavigationView {
             ZStack {
                 List {
-                    ForEach(playlists) { playlist in
-                        NavigationLink(destination: DetailPlaylistView(playlist: playlist)) {
-                            PlaylistRow(playlist: playlist, isShared: hasOnePart(playlist: playlist), isSharing: isSharing(playlist: playlist))
-                                .alert(isPresented: self.$showingDeleteAlert) {
-                                    Alert(title: Text("Are you sure you want to delete?".localized()),
-                                          message: Text("If you are the owner of a shared playlist, all participants will no longer have access.".localized()),
-                                          primaryButton: .destructive(Text("Delete".localized())) {
-                                        self.delete(at: self.toBeDeleted!)
-                                        self.toBeDeleted = nil
-                                    }, secondaryButton: .cancel() {
-                                        self.toBeDeleted = nil
-                                    }
-                                    )
-                                }
-                        }
-                    }
-                    .onDelete(perform: deleteRow)
+                    listPlaylists
                 }
                 .onAppear {
-                    UserDefaults.standard.set(0, forKey: "badgeplaylists")
-                    cloudViewModel.resetBadgePlaylists()
+                    resetBadges()
                 }
                 .navigationTitle("Playlists".localized())
                 .toolbar {
                     ToolbarItemGroup(placement: .navigationBarTrailing) {
-                        Button {
-                            showingAddPlaylistSheet.toggle()
-                        } label: {
-                            Image(systemName: "plus").imageScale(.large)
-                        }
-                        .sheet(isPresented: $showingAddPlaylistSheet) {
-                            AddPlaylistSheet()
-                        }
+                        addPlaylistButton
                     }
                 }
                 if playlists.count == 0 {
                     noPlaylistPrompt
                 }
             }
-            .alert("Invalid Permission".localized(), isPresented: $showNoPermissionsAlert) {
+            .sheet(isPresented: $presentAddPlaylistSheet) {
+                AddPlaylistSheet()
+            }
+            .alert("Invalid Permission".localized(), isPresented: $presentNoPermissionsAlert) {
                 Button("OK".localized(), role: .cancel) { }
             } message: {
                 Text("Only the owner can delete. If you would like to leave the shared playlist, please tap the person icon and choose yourself.".localized())
@@ -77,10 +53,86 @@ struct PlaylistView: View {
         .navigationViewStyle(.automatic)
     }
     
+    // MARK: - Sub Views
+    
+    private var listPlaylists: some View {
+        ForEach(playlists) { playlist in
+            NavigationLink(destination: DetailPlaylistView(playlist: playlist)) {
+                PlaylistRow(playlist: playlist,
+                            isShared: hasOnePart(playlist: playlist),
+                            isSharing: isSharing(playlist: playlist))
+                    .alert(isPresented: self.$presentDeleteAlert) {
+                        removePlaylistAlert
+                    }
+            }
+        }
+        .onDelete(perform: deleteRow)
+    }
+    
+    private var addPlaylistButton: some View {
+        Button {
+            presentAddPlaylistSheet.toggle()
+        } label: {
+            Image(systemName: "plus").imageScale(.large)
+        }
+    }
+    
+    private var noPlaylistPrompt: some View {
+        VStack(spacing: 6) {
+            noPlaylistPromptTitle
+            noPlaylistPromptSubtitle
+        }
+    }
+    
+    private var noPlaylistPromptTitle: some View {
+        HStack {
+            Spacer()
+            Text("No Playlists Here Yet!".localized())
+            Spacer()
+        }
+    }
+    
+    private var noPlaylistPromptSubtitle: some View {
+        HStack {
+            Spacer()
+            HStack {
+                Text("Add Some With The".localized())
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Image(systemName: "plus")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Text("Button Above".localized())
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            Spacer()
+        }
+    }
+    
+    private var removePlaylistAlert: Alert {
+        Alert(title: Text("Are you sure you want to delete?".localized()),
+              message: Text("If you are the owner of a shared playlist, all participants will no longer have access.".localized()),
+              primaryButton: .destructive(Text("Delete".localized())) {
+            self.delete(at: self.toBeDeleted!)
+            self.toBeDeleted = nil
+        }, secondaryButton: .cancel() {
+            self.toBeDeleted = nil
+        }
+        )
+    }
+    
+    // MARK: - Functions
+    
+    private func resetBadges() {
+        UserDefaults.standard.set(0, forKey: "badgeplaylists")
+        cloudViewModel.resetBadgePlaylists()
+    }
+    
     private func hasOnePart(playlist: Playlist) -> Bool {
         var isShared = false
-        if stack.isShared(object: playlist) {
-            let share = stack.getShare(playlist)
+        if CoreDataStack.shared.isShared(object: playlist) {
+            let share = CoreDataStack.shared.getShare(playlist)
             share?.participants.forEach { participant in
                 if (participant.acceptanceStatus == .accepted && participant.role != .owner) {
                     isShared = true
@@ -91,37 +143,12 @@ struct PlaylistView: View {
     }
     
     private func isSharing(playlist: Playlist) -> Bool {
-        return stack.isShared(object: playlist)
-    }
-    
-    private var noPlaylistPrompt: some View {
-        VStack(spacing: 6) {
-            HStack {
-                Spacer()
-                Text("No Playlists Here Yet!".localized())
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                HStack {
-                    Text("Add Some With The".localized())
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                    Image(systemName: "plus")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                    Text("Button Above".localized())
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-                Spacer()
-            }
-        }
+        return CoreDataStack.shared.isShared(object: playlist)
     }
     
     private func deleteRow(at indexSet: IndexSet) {
         self.toBeDeleted = indexSet
-        self.showingDeleteAlert = true
+        self.presentDeleteAlert = true
         let generator = UINotificationFeedbackGenerator()
         generator.notificationOccurred(.warning)
     }
@@ -130,19 +157,19 @@ struct PlaylistView: View {
         let generator = UINotificationFeedbackGenerator()
         generator.notificationOccurred(.warning)
         offsets.forEach { i in
-            if !stack.isShared(object: playlists[i]) {
+            if !CoreDataStack.shared.isShared(object: playlists[i]) {
                 if (playlists[i].spotArr.count > 0) {
                     for place in playlists[i].spotArr {
                         place.playlist = nil
                     }
                 }
-                stack.delete(playlists[i])
-            } else if stack.isOwner(object: playlists[i]) {
-                stack.delete(playlists[i])
+                CoreDataStack.shared.delete(playlists[i])
+            } else if CoreDataStack.shared.isOwner(object: playlists[i]) {
+                CoreDataStack.shared.delete(playlists[i])
             } else {
                 let generator = UINotificationFeedbackGenerator()
                 generator.notificationOccurred(.error)
-                showNoPermissionsAlert = true
+                presentNoPermissionsAlert = true
             }
             return
         }
