41308b9a604446bc02586a8d47446d314d75539e
isaacpasc <72172489+isaacpasc@users.noreply.github.com>
2022-06-18 13:09:08 -0700

    refactor

--- My Spot/View/NotificationView.swift
+++ My Spot/View/NotificationView.swift
@@ -22,117 +22,180 @@ struct NotificationView: View {
     
     var body: some View {
         NavigationView {
-            if !cloudViewModel.notiNewSpotOn && cloudViewModel.notificationSpots.isEmpty {
-                noNotification
-            } else if cloudViewModel.notificationSpots.isEmpty && !isFetching {
-                noSpotsMessage
-            } else {
-                stack
-                    .navigationTitle("Notifications".localized())
-                    .navigationViewStyle(.stack)
-                    .toolbar {
-                        ToolbarItemGroup(placement: .navigationBarTrailing) {
-                            Button {
-                                let generator = UINotificationFeedbackGenerator()
-                                generator.notificationOccurred(.warning)
-                                showingAlert.toggle()
-                            } label: {
-                                Text("Remove All".localized())
-                                    .foregroundColor(.red)
-                            }
-                            .disabled(isFetching || cloudViewModel.notificationSpots.isEmpty)
-                        }
-                        ToolbarItemGroup(placement: .navigationBarLeading) {
-                            Button {
-                                presentationMode.wrappedValue.dismiss()
-                            } label: {
-                                Text("Done".localized())
-                            }
-                        }
-                    }
-            }
+            notificationMainView
         }
         .alert("Are you sure you want to remove all notifications?".localized(), isPresented: $showingAlert) {
             Button("Remove All".localized(), role: .destructive) {
-                cloudViewModel.notificationSpots = []
-                if UserDefaults.standard.valueExists(forKey: "newSpotNotiRecords") {
-                    UserDefaults.standard.set([], forKey: "newSpotNotiRecords")
-                }
+                removeNotificationSpots()
             }
         }
         .onAppear {
-            UIApplication.shared.applicationIconBadgeNumber = 0
-            UserDefaults.standard.set(0, forKey: "badge")
-            badgeNum = 0
-            cloudViewModel.resetBadgeNewSpots()
+            removeBadges()
             reloadData()
+        }
+    }
+    
+    // MARK: - Sub Views
+    
+    @ViewBuilder
+    private var notificationMainView: some View {
+        if !cloudViewModel.notiNewSpotOn && cloudViewModel.notificationSpots.isEmpty {
+            noNotification
+        } else if cloudViewModel.notificationSpots.isEmpty && !isFetching {
+            noSpotsMessage
+        } else {
+            notificationSpots
+        }
+    }
+    
+    private var notificationSpots: some View {
+        ZStack {
+            listSpots
+            if isFetching {
+                loadingSpotsSpinner
+            }
+            if hasError {
+                errorLoadingSpots
+            }
+        }
+        .navigationTitle("Notifications".localized())
+        .navigationViewStyle(.stack)
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                removeAllButton
+            }
+            ToolbarItemGroup(placement: .navigationBarLeading) {
+                doneButton
+            }
+        }
+    }
+    
+    private var removeAllButton: some View {
+        Button {
+            let generator = UINotificationFeedbackGenerator()
+            generator.notificationOccurred(.warning)
+            showingAlert.toggle()
+        } label: {
+            Text("Remove All".localized())
+                .foregroundColor(.red)
+        }
+        .disabled(isFetching || cloudViewModel.notificationSpots.isEmpty)
+    }
+    
+    private var doneButton: some View {
+        Button {
+            presentationMode.wrappedValue.dismiss()
+        } label: {
+            Text("Done".localized())
+        }
+    }
+    
+    private var loadingSpotsSpinner: some View {
+        ProgressView("Loading Spots".localized())
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(UIColor.systemBackground))
+            )
+    }
+    
+    private var errorLoadingSpots: some View {
+        VStack {
+            errorLoadingSpotsTitle
+            errorLoadingSpotsSubtitle
+        }
+        .padding(.vertical)
+        .padding(.horizontal, 2)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color(UIColor.systemBackground))
+        )
+        .onAppear {
+            checkIfNotificationSpotsHaveLoaded()
+        }
+    }
+    
+    private var errorLoadingSpotsTitle: some View {
+        HStack {
+            Spacer()
+            Text("Unable to load spots".localized())
+                .foregroundColor(.gray)
+                .font(.headline)
+            Spacer()
+        }
+    }
+    
+    private var errorLoadingSpotsSubtitle: some View {
+        HStack {
+            Spacer()
+            Text("Try refreshing or checking your internet connection".localized())
+                .foregroundColor(.gray)
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+            Spacer()
         }
     }
     
     private var noNotification: some View {
         VStack(spacing: 6) {
-            HStack {
-                Spacer()
-                Text("No Notifications".localized())
+            noNotificationTitle
+            noNotificationSubtitle
+        }
+    }
+    
+    private var noNotificationTitle: some View {
+        HStack {
+            Spacer()
+            Text("No Notifications".localized())
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            Spacer()
+        }
+    }
+    
+    private var noNotificationSubtitle: some View {
+        HStack {
+            Spacer()
+            Button {
+                goToSettings = true
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("Please go to settings to enable notifications".localized())
+                    .font(.subheadline)
+                    .foregroundColor(.blue)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal)
-                Spacer()
             }
-            HStack {
-                Spacer()
-                Button {
-                    goToSettings = true
-                    presentationMode.wrappedValue.dismiss()
-                } label: {
-                    Text("Please go to settings to enable notifications".localized())
-                        .font(.subheadline)
-                        .foregroundColor(.blue)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal)
-                }
-                Spacer()
-            }
+            Spacer()
         }
     }
     
     private var noSpotsMessage: some View {
         VStack(spacing: 6) {
-            HStack {
-                Spacer()
-                Text("No Notifications".localized())
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                Text("Your notifications will arrive here".localized())
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-                Spacer()
-            }
+            noSpotsMessageTitle
+            noSpotsMessageSubtitle
         }
     }
     
-    private func reloadData() {
-        guard let recordids = UserDefaults.standard.stringArray(forKey: "newSpotNotiRecords") else {
-            return
+    private var noSpotsMessageTitle: some View {
+        HStack {
+            Spacer()
+            Text("No Notifications".localized())
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            Spacer()
         }
-        if recordids.isEmpty { return }
-        Task {
-            isFetching = true
-            cloudViewModel.notificationSpots.removeAll()
-            do {
-                for recordid in recordids {
-                    try await cloudViewModel.fetchNotificationSpots(recordid: recordid)
-                }
-                isFetching = false
-            } catch {
-                isFetching = false
-                hasError = true
-            }
+    }
+    
+    private var noSpotsMessageSubtitle: some View {
+        HStack {
+            Spacer()
+            Text("Your notifications will arrive here".localized())
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            Spacer()
         }
     }
     
@@ -165,53 +228,19 @@ struct NotificationView: View {
         }
     }
     
-    private var stack: some View {
-        ZStack {
-            listSpots
-            if (isFetching) {
-                ZStack {
-                    ProgressView("Loading Spots".localized())
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color(UIColor.systemBackground))
-                        )
-                }
-            }
-            if hasError {
-                VStack {
-                    HStack {
-                        Spacer()
-                        Text("Unable to load spots".localized())
-                            .foregroundColor(.gray)
-                            .font(.headline)
-                        Spacer()
-                    }
-                    HStack {
-                        Spacer()
-                        Text("Try refreshing or checking your internet connection".localized())
-                            .foregroundColor(.gray)
-                            .font(.subheadline)
-                            .multilineTextAlignment(.center)
-                        Spacer()
-                    }
-                }
-                .padding(.vertical)
-                .padding(.horizontal, 2)
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color(UIColor.systemBackground))
-                )
-                .onAppear {
-                    if cloudViewModel.notificationSpots.count != 0 {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            withAnimation {
-                                hasError.toggle()
-                            }
-                        }
-                    }
-                }
-            }
+    // MARK: - Functions
+    
+    private func removeBadges() {
+        UIApplication.shared.applicationIconBadgeNumber = 0
+        UserDefaults.standard.set(0, forKey: "badge")
+        badgeNum = 0
+        cloudViewModel.resetBadgeNewSpots()
+    }
+    
+    private func removeNotificationSpots() {
+        cloudViewModel.notificationSpots = []
+        if UserDefaults.standard.valueExists(forKey: "newSpotNotiRecords") {
+            UserDefaults.standard.set([], forKey: "newSpotNotiRecords")
         }
     }
     
@@ -219,5 +248,33 @@ struct NotificationView: View {
         guard var recordid = UserDefaults.standard.stringArray(forKey: "newSpotNotiRecords") else { return }
         recordid.remove(atOffsets: offsets)
         cloudViewModel.notificationSpots.remove(atOffsets: offsets)
+    }
+    
+    private func reloadData() {
+        guard let recordids = UserDefaults.standard.stringArray(forKey: "newSpotNotiRecords") else {
+            return
+        }
+        if recordids.isEmpty { return }
+        Task {
+            isFetching = true
+            cloudViewModel.notificationSpots.removeAll()
+            do {
+                for recordid in recordids {
+                    try await cloudViewModel.fetchNotificationSpots(recordid: recordid)
+                }
+                isFetching = false
+            } catch {
+                isFetching = false
+                hasError = true
+            }
+        }
+    }
+    
+    private func checkIfNotificationSpotsHaveLoaded() {
+        if cloudViewModel.notificationSpots.count != 0 {
+            withAnimation {
+                hasError = false
+            }
+        }
     }
 }
